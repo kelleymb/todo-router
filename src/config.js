8b76f92f916d4e7fb1de61cc9bd1bed26fdb3ad3
Kelleymb <kelleybuccieri@gmail.com>
2020-08-18 20:40:18 -0400

    7/10 passing

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://todo_user@localhost/todos'
+  DB_URL: process.env.DB_URL || 'postgresql://testing_user@localhost/todo-router',
+  TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://testing_user@localhost/todo-router-test'
 }
